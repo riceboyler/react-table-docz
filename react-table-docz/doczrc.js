21aa554cb15240f44cd4e88a78f578dad0c00cf7
Jason Clark <jason.clark@tcnbroadcasting.com>
2020-03-16 15:41:15 -0600

    Initial commit

---
+++ react-table-docz/doczrc.js
@@ -0,0 +1,20 @@
+export default {
+  title: 'React Table Documentation',
+  description: 'Documentation for the react-table library built in Docz',
+  menu: [
+    'Getting Started',
+    'Installation',
+    'Concepts',
+    'Quick Start',
+    {
+      name: 'Examples',
+      menu: ['Simple', 'Complex', 'Controlled', 'UI/Rendering'],
+    },
+    { name: 'API', menu: ['Overview', 'useTable'] },
+    'FAQ',
+    'Contributing',
+    'Code of Conduct',
+    'Changelog',
+    'TypeScript',
+  ],
+}
